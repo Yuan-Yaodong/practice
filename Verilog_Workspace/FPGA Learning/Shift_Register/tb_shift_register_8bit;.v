26b9f5203b415e5444e23108a62c4993ea5ea0bb
Yuan-Yaodong <yaodong@ruri.waseda.jp>
2023-11-04 22:40:21 +0900

    update vcd

--- Verilog_Workspace/FPGA Learning/Shift_Register/tb_shift_register_8bit;.v
+++ Verilog_Workspace/FPGA Learning/Shift_Register/tb_shift_register_8bit;.v
@@ -1,5 +1,5 @@
-`timescale  1ns/1ns   // 单位/精度
-module tb_shift_register_8bit();
+`timescale 1ns / 1ns  // 单位/精度
+module tb_shift_register_8bit ();
 
   reg clk;
   reg reset;
@@ -16,7 +16,8 @@ module tb_shift_register_8bit();
 
   // 初始化输入信号
   initial begin
-    $monitor("%t CLK = %b,Reset = %b,shift_left = %b,shift_right = %b,data_in = %b,Data: %b", $time, clk,reset, shift_left, shift_right, data_in, data);
+    $monitor("%t CLK = %b,Reset = %b,shift_left = %b,shift_right = %b,data_in = %b,Data: %b",
+             $time, clk, reset, shift_left, shift_right, data_in, data);
 
     clk <= 0;
     reset <= 0;
@@ -30,25 +31,27 @@ module tb_shift_register_8bit();
     // 输入数据并进行移位操作
     #10 data_in <= 1;
     shift_left <= 1;
-    #30 
-    #10 data_in <= 0;
+    #30 #10 data_in <= 0;
     #20 reset <= 0;
     #20
 
-    // 结束仿真
-    $finish;
+      // 结束仿真
+      $finish;
   end
 
-
-// 实例化被测试的移位寄存器模块
-shift_register_8bit t_shift_reg(
-    .clk(clk),
-    .reset(reset),
-    .shift_left(shift_left),
-    .shift_right(shift_right),
-    .data_in(data_in),
-    .data(data)
-);
+  initial begin
+    $dumpfile("tb_sreg.vcd");
+    $dumpvars(0, tb_shift_register_8bit);
+  end
+  // 实例化被测试的移位寄存器模块
+  shift_register_8bit t_shift_reg (
+      .clk(clk),
+      .reset(reset),
+      .shift_left(shift_left),
+      .shift_right(shift_right),
+      .data_in(data_in),
+      .data(data)
+  );
 
 
 endmodule
