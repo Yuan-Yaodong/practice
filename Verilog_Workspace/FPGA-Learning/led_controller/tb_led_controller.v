e5dded3883ea461c229fe9b97b3629235517d681
Yuan-Yaodong <yaodong@ruri.waseda.jp>
2023-11-11 14:59:36 +0900

    add constraints files and some updates
    -- using parameter CNT_MAX in Testbench to simulate.

--- Verilog_Workspace/FPGA-Learning/led_controller/tb_led_controller.v
+++ Verilog_Workspace/FPGA-Learning/led_controller/tb_led_controller.v
@@ -3,7 +3,8 @@
 module tb_led_controller;
     
     // led_controller Parameters
-    parameter PERIOD = 20;
+    parameter PERIOD  = 20;
+    parameter CNT_MAX = 26'd10;    // count every 200ns, clock is 50MHz
     
     // led_controller Inputs
     reg   sys_clk;
@@ -11,37 +12,39 @@ module tb_led_controller;
     reg [1:0] key;
     
     wire [1:0] led;
-
-initial
-begin
-    forever #(PERIOD/2)  sys_clk = ~sys_clk;
-end
-
-
-led_controller u_led_controller (
-    .sys_clk                 ( sys_clk          ),
-    .sys_rst_n               ( sys_rst_n        ),
-    .key                     ( key        [1:0] ),
-    .led                     ( led        [1:0] )
-);
-
-initial
-begin
-    sys_clk <= 1'b0;
-    sys_rst_n <= 1'b0;
-    key <= 2'b11;
-    u_led_controller.led_flag <= 1'b0;
-    #200
-    sys_rst_n <= 1'b1;
-    #1000
-    key <= 2'b10;
-    #10000
-    key <= 2'b01;
-    #10000
-    key <= 2'b00;
-    #10000
-    $finish;
-
-end
-
+    
+    initial
+    begin
+        forever #(PERIOD/2)  sys_clk = ~sys_clk;
+    end
+    
+    
+    led_controller #(
+    .CNT_MAX (CNT_MAX))
+    u_led_controller (
+    .sys_clk                 (sys_clk),
+    .sys_rst_n               (sys_rst_n),
+    .key                     (key        [1:0]),
+    .led                     (led        [1:0])
+    );
+    
+    initial
+    begin
+        sys_clk                   <= 1'b0;
+        sys_rst_n                 <= 1'b0;
+        key                       <= 2'b11;
+        u_led_controller.led_flag <= 1'b0;
+        #200
+        sys_rst_n <= 1'b1;
+        #1000
+        key <= 2'b10;
+        #1000
+        key <= 2'b01;
+        #1000
+        key <= 2'b00;
+        #1000
+        $finish;
+        
+    end
+    
 endmodule
